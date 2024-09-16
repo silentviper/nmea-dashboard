ef597d2898d6fb2fc3d261fb31b63cdf8b488b24
James McGinness <jamesmcginness@Jamess-MacBook-Pro.local>
2024-09-16 15:48:05 -0700

    updated to make it run next-server

---
+++ index.ts
@@ -0,0 +1,158 @@
+import { Plugin, PluginServerApp } from '@signalk/server-api';
+import { createServer } from 'http';
+import { parse } from 'url';
+import next from 'next';
+import dgram from 'dgram';
+import os from 'os';
+import debugLib from 'debug';
+
+const id = 'signalk-nmea-dashboard';
+const debug = debugLib(id);
+const PUBLISH_PORT = 2053;
+const MULTICAST_GROUP_IP = '239.2.1.1';
+
+const tiles = [
+	{
+		Source: 'SignalK',
+		IP: '192.168.x.x',
+		FeatureName: 'Signal K webapps',
+		Name: 'Dashboard',
+		Description: 'NMEA Dashboard',
+		Icon: 'http://192.168.x.x:3000/assets/img/logo.png',
+		URL: 'http://192.168.x.x:3000/nmea-dashboard/',
+		MenuText: 'Signal K Menu Text',
+	},
+];
+
+const updateTilesIP = () => {
+	const interfaces = os.networkInterfaces();
+	for (const interfaceName in interfaces) {
+		const addresses = interfaces[interfaceName];
+		for (const addressInfo of addresses) {
+			if (
+				addressInfo.family === 'IPv4' &&
+				!addressInfo.internal &&
+				interfaceName === 'eth0'
+			) {
+				tiles[0].IP = addressInfo.address;
+				tiles[0].Icon = `http://${addressInfo.address}:3000/assets/img/logo.png`;
+				tiles[0].URL = `http://${addressInfo.address}:3000/nmea-dashboard/`;
+			}
+		}
+	}
+};
+
+let intervalid: NodeJS.Timeout;
+
+module.exports = (app: PluginServerApp): Plugin => {
+	const plugin: Plugin = {
+		id: 'my-signalk-plugin',
+		name: 'My Great Plugin',
+		start: async (settings, restartPlugin) => {
+			// update tiles object with eth0 ip address
+			updateTilesIP();
+
+			// start up code goes here.
+			const nextApp = next({
+				dev: settings.env !== 'production',
+			});
+
+			const routes = require('./routes');
+			const handler = routes.getRequestHandler(nextApp);
+
+			try {
+				await nextApp.prepare();
+				createServer((req, res) => {
+					const parsedUrl = parse(req.url!, true);
+					handler(req, res, parsedUrl);
+				}).listen(settings.port, (err) => {
+					if (err) throw err;
+					debug(`Ready on localhost:${settings.port}`);
+				});
+			} catch (err) {
+				console.error('Error starting Next.js server:', err);
+			}
+
+			intervalid = setInterval(() => publishToNavico(tiles), 10 * 1000);
+		},
+		stop: () => {
+			// shutdown code goes here.
+			clearInterval(intervalid);
+		},
+		schema: {
+			type: 'object',
+			properties: {
+				env: {
+					type: 'string',
+					title: 'NextJS Environment',
+					default: 'production',
+				},
+				port: {
+					type: 'number',
+					title: 'NextJS Server Port',
+					default: 3001,
+				},
+			},
+		},
+	};
+
+	return plugin;
+};
+
+const getPublishMessage = (tile) => {
+	return JSON.stringify({
+		Version: '1',
+		Source: tile['Source'],
+		IP: tile['IP'],
+		FeatureName: tile['FeatureName'],
+		Text: [
+			{
+				Language: 'en',
+				Name: tile['Name'],
+				Description: tile['Description'],
+			},
+		],
+		Icon: tile['Icon'],
+		URL: tile['URL'],
+		OnlyShowOnClientIP: 'true',
+		BrowserPanel: {
+			Enable: true,
+			ProgressBarEnable: true,
+			MenuText: [
+				{
+					Language: 'en',
+					Name: tile['MenuText'],
+				},
+			],
+		},
+	});
+};
+
+const send = (msg, fromAddress, toAddress, port) => {
+	const socket = dgram.createSocket('udp4');
+	socket.once('listening', () => {
+		socket.send(msg, port, toAddress, () => {
+			socket.close();
+			debug(`${fromAddress}=>${toAddress} @${port} ${msg}`);
+		});
+	});
+	socket.bind(PUBLISH_PORT, fromAddress);
+};
+
+const publishToNavico = (tiles) => {
+	for (const [name, infos] of Object.entries(os.networkInterfaces())) {
+		for (const addressInfo of infos || []) {
+			tiles.forEach((tile) => {
+				if (addressInfo.address == tile['IP']) {
+					debug('tile: %j', JSON.stringify(tile));
+					send(
+						getPublishMessage(tile),
+						addressInfo.address,
+						MULTICAST_GROUP_IP,
+						PUBLISH_PORT
+					);
+				}
+			});
+		}
+	}
+};
